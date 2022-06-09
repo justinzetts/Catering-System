3826ccacb0e63fa4c4b7540a1eacf6542474b724
Justin Zetts <justinzetts@gmail.com>
2022-06-09 12:01:58 -0400

    Built out UserInterface structure, created classes for CateringItems

--- module-1_Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ module-1_Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -14,13 +14,69 @@ namespace Capstone.Classes
 
         public void RunMainMenu()
         {
-            bool done = false;
+            bool quitMainMenu = false;
 
-            while (!done)
+            while (!quitMainMenu)
             {
-                Console.WriteLine("Put details of your user interface here");
+                Console.WriteLine("(1) Display Catering Items");
 
-                Console.ReadLine();
+                Console.WriteLine("(2) Order");
+
+                Console.WriteLine("(3) Quit");
+
+                string mainMenuChoice = Console.ReadLine();
+
+                if(mainMenuChoice == "(1)" || mainMenuChoice == "1" || mainMenuChoice == "one" || mainMenuChoice == "One")
+                {
+
+                }
+                    // do DisplayCateringItems() method, bro
+
+                if(mainMenuChoice == "(2)" || mainMenuChoice == "2" || mainMenuChoice == "two" || mainMenuChoice == "Two")
+                {
+                    public void RunOrderMenu()
+                    {
+
+                        bool quitOrderMenu = false;
+
+                        while (!quitOrderMenu)
+                        {
+                            Console.WriteLine("(1) Add Money");
+
+                            Console.WriteLine("(2) Select Products");
+
+                            Console.WriteLine("(3) Complete Transaction");
+
+                            string orderMenuChoice = Console.ReadLine();
+
+                            if (orderMenuChoice == "(1)" || orderMenuChoice == "1" || orderMenuChoice == "one" || orderMenuChoice == "One")
+                            {
+                                
+
+                                // do AddMoney() method, bro
+
+
+                            }
+
+                            if (orderMenuChoice == "(2)" || orderMenuChoice == "2" || orderMenuChoice == "two" || orderMenuChoice == "Two")
+                            {
+                                // do SelectProducts() method, bro
+                            }
+
+                            if (orderMenuChoice == "(3)" || orderMenuChoice == "3" || orderMenuChoice == "three" || orderMenuChoice == "Three")
+                            {
+                                quitOrderMenu = true;
+                            }
+                        }
+
+
+                    }
+                }
+
+                if (mainMenuChoice == "(3)" || mainMenuChoice == "3" || mainMenuChoice == "three" || mainMenuChoice == "Three")
+                {
+                    quitMainMenu = true;
+                }
             }
         }
     }
