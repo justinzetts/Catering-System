1c02d76d91047bad6b48a33bf725b1a8493dd516
Merritt Weirick <weirickmerritt@gmail.com>
2022-06-09 13:22:44 -0400

    Add Money feature added

--- module-1_Mini-Capstone/Capstone/Classes/CateringSystem.cs
+++ module-1_Mini-Capstone/Capstone/Classes/CateringSystem.cs
@@ -9,9 +9,23 @@ namespace Capstone.Classes
     /// </summary>
     public class CateringSystem
     {
-        private readonly List<CateringItem> items = new List<CateringItem>();
-        
+        public CateringSystem()
+        {
 
+        }
+        private readonly List<CateringItem> items = new List<CateringItem>();
+
+        public double Balance { get; set; } = 0;
+        public void AddMoney(int deposit)
+        {
+            
+            if ((Balance+deposit) <= 1000)
+            {
+                Balance += deposit;
+            }
+            
+
+        }
    
     }
 }
