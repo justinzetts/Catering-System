3127fe6b9b3901833c2cda19a65431dd899dea61
Merritt Weirick <weirickmerritt@gmail.com>
2022-06-10 10:48:08 -0400

    Moved all CW's and CRL's back to user interface, fixed negative number on add money feature

--- module-1_Mini-Capstone/Capstone/Classes/CateringSystem.cs
+++ module-1_Mini-Capstone/Capstone/Classes/CateringSystem.cs
@@ -16,16 +16,25 @@ namespace Capstone.Classes
         private Dictionary<string, CateringItem> items = new Dictionary<string, CateringItem>();
         public List<CateringItem> purchasedItems = new List<CateringItem>();
         public double Balance { get; set; } = 0;
-        public void AddMoney(int deposit)
+        public string AddMoney(int deposit)
         {
 
-            if ((Balance + deposit) <= 1000 && Balance + deposit >= 0)
+            if (deposit < 0)
+            {
+                return $"The feature says add money not subratact, this ain't a bank G";
+            }
+            else if ((Balance + deposit) <= 1000 && Balance + deposit >= 0)
             { // Merritt said I could break this
                 Balance += deposit;
+                return $"Your new Balance is: ${Balance}";
             }
             else if(Balance + deposit > 1000)
             {
-                Console.WriteLine( "I already told you to keep it under $1000, stop wasting my time and try again.");
+                return "I already told you to keep it under $1000, stop wasting my time and try again.";
+            }
+            else 
+            {
+                return "I blame John for this.";
             }
 
         }
@@ -34,51 +43,57 @@ namespace Capstone.Classes
             items.Add(item.ID, item);
         }
 
-        public void DisplayCateringItems()
+        public void BuildCateringMenu(List<string> menu)
         {
+            
             foreach (KeyValuePair<string, CateringItem> kvp in items)
             {
                 if (kvp.Value.Quantity == 0)
                 {
-                    Console.WriteLine($"{kvp.Value.Name} ~~ {kvp.Value.ID} ~~ ${kvp.Value.Price} ~~ SOLD OUT ");
+                     menu.Add($"{kvp.Value.Name} ~~ {kvp.Value.ID} ~~ ${kvp.Value.Price} ~~ SOLD OUT ");
                 }
-                Console.WriteLine($"{kvp.Value.Name} ~~ {kvp.Value.ID} ~~ ${kvp.Value.Price} ~~ {kvp.Value.Quantity} ");
+                 menu.Add($"{kvp.Value.Name} ~~ {kvp.Value.ID} ~~ ${kvp.Value.Price} ~~ {kvp.Value.Quantity} ");
                 // name, id, price, quantity
             }
+             
         }
 
-        public void SelectProduct(string choice, int amountToPurchase)
+        public string SelectProduct(string choice, int amountToPurchase)
         {
             bool choiceIsInDictionary = items.ContainsKey(choice);
-            int itemQuantity = items[choice].Quantity;
+            int amountAvailable = items[choice].Quantity;
             double costOfPurchase = items[choice].Price * amountToPurchase;
-            bool purchaseComplete = false;
+            
 
-            while (!purchaseComplete)
+
+            if (choiceIsInDictionary && (amountAvailable - amountToPurchase) >= 0 && (Balance >= costOfPurchase))
             {
-                if (choiceIsInDictionary && (itemQuantity - amountToPurchase) >= 0 && (Balance >= costOfPurchase))
-                {
-                    //if purchase will be successful
-                    items[choice].Quantity -= amountToPurchase;
-                    Balance -= (costOfPurchase);
-                    purchasedItems.Add(items[choice]);
-                    purchaseComplete = true;
+                //if purchase will be successful
+                items[choice].Quantity -= amountToPurchase;
+                Balance -= (costOfPurchase);
+                purchasedItems.Add(items[choice]);
+                return $"Your purchase of {choice} was successful, your new balance is ${Balance}";
+                
 
-                }
-                else if (!choiceIsInDictionary)
-                {
-                    Console.WriteLine($"Sorry your selected choice of {choice} does not exist, please select product listed above.");
-                    purchaseComplete = true;
-                }
-                else if (itemQuantity <= 0)
-                {
-                    Console.WriteLine($"We are currently out of {choice}, please select different item");
+            }
+            else if (!choiceIsInDictionary) // item does not exist
+            {
+                return $"Sorry your selected choice of {choice} does not exist, please select product listed above.";
+               
+            }
+            else if (amountAvailable <= 0) //Item sold out
+            {
+                return $"We are currently out of {choice}, please select different item";
+                
+            }
+            else if ((amountAvailable - amountToPurchase) < 0) //insufficient stock amount
+            {
+                return "Insufficient stock for amount requested.";  
 
-                }
-                else if ((itemQuantity - amountToPurchase) < 0)
-                {
-                    Console.WriteLine($"Insufficient stock for amount requested.");
-                }
+            }
+            else
+            {
+                return "random thing we didn't think about";
             }
 
         }
